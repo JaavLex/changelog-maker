a71c8a9602753a3a4c0ed7587f0f18edf3303a23
TacticsCH <alexandre.javet@hotmail.com>
2021-07-20 16:04:10 +0200

    [feat] (WIP) Attempt to write all commits in a .txt file
    ----------
    - ISSUES -
    ----------
    
    - Github's API request rate is limited for non-fully authentificated requests (What a fricking bummer),
    so now I have to input my API token and make a system to extract secrets from keybase or something etc...
    That will for sure delay any progress on the rest of the project for at least half a day at my estimation.
    
    - I can't verify if my code ACTUALLY works or not because of the issue mentionned above. So we'll have the
    incertitude until I fix this.
    
    - My code to change the API's page is a bit junky, but I think it'll work for now. Though I will still need
    to improve this. It'll become an issue when when have tens of thousands commits in a repository.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const superagent = require("superagent");
+const fs = require("fs");
 
 async function GetApiInfo() {
-  let { body } = await superagent
-      .get('https://api.github.com/repos/GinierClasses/holydraw/commits')
-      .set('User-Agent', 'TacticsCH');
+  do {
+    pgnumber = 1;
+    antpage = true;
 
-      body.forEach(i => console.log("COMMIT : " + i.commit.message + " | BY : " + i.commit.author.name + " | ON : " + i.commit.author.date));
+    let { body } = await superagent
+        .get(`https://api.github.com/repos/GinierClasses/holydraw/commits?per_page=100?page=${pgnumber}`)
+        .set('User-Agent', 'TacticsCH');
+  
+    // body.forEach(i => console.log("COMMIT : " + i.commit.message + " | BY : " + i.commit.author.name + " | ON : " + i.commit.author.date))
+    body.forEach(i => fs.writeFileSync('./commits.txt', "COMMIT : " + i.commit.message + " | BY : " + i.commit.author.name + " | ON : " + i.commit.author.date))
+    
+    let { test } = await superagent
+    .get(`https://api.github.com/repos/GinierClasses/holydraw/commits?per_page=100?page=${pgnumber + 1}`)
+    .set('User-Agent', 'TacticsCH');
+
+    if (test == null) {
+      antpage = true;
+      pgnumber++;
+    } else {
+      antpage = false;
+    }
+  } while (antpage)
 }
 
 GetApiInfo();
