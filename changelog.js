34aa68c0a79168b4fbe4b56a836db0878eb5add4
Alexandre Javet <alexandre.javet@hotmail.com>
2021-07-23 13:49:23 +0000

    [feat] Now sorts commits !
    - Still looking how to remove the "feat" and "fix" from the messages but I'll soon figure it out

--- changelog.js
+++ changelog.js
@@ -1,55 +1,28 @@
-// Version pour lister les détails des commits du projet zuzu59/NodeMCU_Lua
-// zf200825.1158
-var page = 1;
-var previousRequest;
+async function getCommits(repoUrl, numberPage) {
+  const repoCommits = [];
 
-console.log("Start");
-
-function changeForward(prevOrNext) {
-  if (prevOrNext == true) {
-    page++;
-    loadPage(page)
-  } else {
-    page--;
-    loadPage(page)
+  for (let i = 1; i < numberPage; i++) {
+    const repoContent = await fetch(repoUrl+"?page="+i);
+    const jsonCommits = await repoContent.json();
+    repoCommits.push(...jsonCommits);
   }
+
+  return repoCommits;
 }
 
-function loadPage(nextorprevious) {
-  var myRequest = new Request(`https://api.github.com/repos/GinierClasses/holydraw/commits?page=${nextorprevious}`, {
-    method: 'GET',
-    headers: {
-      'User-Agent': 'Mozilla/5.0',
-    },
-    });
+async function sortCommits() {
+  const rawCommits = await getCommits("https://api.github.com/repos/GinierClasses/holydraw/commits", 30);
+  const commitMessages = rawCommits.map((item) => item.commit.message);
+  const features = commitMessages.filter((message) => message.match(/^feat/));
+  const fixes = commitMessages.filter((message) => message.match(/^fix/));
 
-    console.log(myRequest);
-
-    if (myRequest == null) {
-      alert(`Page N#${page} doesn't exist`)
-    } else {
-      fetch(myRequest)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        if (previousRequest == data) {
-          alert(`Page N#${page} doesn't exist`)
-        } else {
-          let newBody = '<h1>NodeMCU_Lua 1640</h1>';
-          newBody += '<a href="https://github.com/zuzu59/NodeMCU_Lua" >https://github.com/zuzu59/NodeMCU_Lua</a><br>';
-          newBody += `<button onclick="changeForward(false)">Previous</button> PAGE N#${page} <button onclick="changeForward(true)">Next</button>`;
-          data.forEach((item, i) => {
-            newBody += '<h3>' + item.commit.author.date + '</h3>';
-            newBody += '<p>' + item.commit.message + '</p>';
-          });
-          document.getElementById("bodyhtml").innerHTML = newBody;
-
-          previousRequest = data;
-        }
-      })
-      .catch(function(err) {
-        console.warn('Something went wrong.', err);
-      });
-    }
+  let newBody = '<h1>Changelog</h1>';
+  newBody += '<a href="https://github.com/TacticsCH/changelog-maker" >https://github.com/TacticsCH/changelog-maker</a><br>';
+  newBody += `<h2>New features</h2>`;
+  newBody += features.join("<br>");
+  newBody += `<h2>Bug fixes</h2>`;
+  newBody += fixes.join("<br>");
+  document.getElementById("bodyhtml").innerHTML = newBody;
 }
+
+
