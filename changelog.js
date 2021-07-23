165ae7a2dc1c23b2a005466ab8fc6cb3e391053f
Alexandre Javet <alexandre.javet@hotmail.com>
2021-07-23 12:07:58 +0000

    [feat] You can now change pages !
    BUG TO FIX (Or not since I will rewrite the code probably) : You can go to negative numbers pages

--- changelog.js
+++ changelog.js
@@ -1,51 +1,55 @@
 // Version pour lister les détails des commits du projet zuzu59/NodeMCU_Lua
 // zf200825.1158
-//
+var page = 1;
+var previousRequest;
 
 console.log("Start");
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-document.getElementById("startbutton").onclick = function() {
-  console.log("TEST");
-  loadPage(1)
-};
 
-page = 1;
-
-document.getElementById("nextbutton").onclick = function() {
-  page++;
-  loadPage(page)
-};
-
-document.getElementById("previousbutton").onclick = function() {
-  page--;
-  loadPage(page)
-};
+function changeForward(prevOrNext) {
+  if (prevOrNext == true) {
+    page++;
+    loadPage(page)
+  } else {
+    page--;
+    loadPage(page)
+  }
+}
 
 function loadPage(nextorprevious) {
-  const myRequest = new Request(`https://api.github.com/repos/GinierClasses/holydraw/commits?page=${nextorprevious}`, {
+  var myRequest = new Request(`https://api.github.com/repos/GinierClasses/holydraw/commits?page=${nextorprevious}`, {
     method: 'GET',
     headers: {
       'User-Agent': 'Mozilla/5.0',
     },
     });
-  
-    fetch(myRequest)
-    .then(function(response) {
-      // The API call was successful!
-      return response.json();
-    })
-    .then(function(data) {
-      let newBody = '<h1>NodeMCU_Lua 1640</h1>';
-      newBody += '<a href="https://github.com/zuzu59/NodeMCU_Lua" >https://github.com/zuzu59/NodeMCU_Lua</a><br>';
-      newBody += `<button id="previousbutton">Previous</button> PAGE N#${page} <button id="nextbutton">Next</button>`;
-      data.forEach((item, i) => {
-        newBody += '<h3>' + item.commit.author.date + '</h3>';
-        newBody += '<p>' + item.commit.message + '</p>';
+
+    console.log(myRequest);
+
+    if (myRequest == null) {
+      alert(`Page N#${page} doesn't exist`)
+    } else {
+      fetch(myRequest)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        if (previousRequest == data) {
+          alert(`Page N#${page} doesn't exist`)
+        } else {
+          let newBody = '<h1>NodeMCU_Lua 1640</h1>';
+          newBody += '<a href="https://github.com/zuzu59/NodeMCU_Lua" >https://github.com/zuzu59/NodeMCU_Lua</a><br>';
+          newBody += `<button onclick="changeForward(false)">Previous</button> PAGE N#${page} <button onclick="changeForward(true)">Next</button>`;
+          data.forEach((item, i) => {
+            newBody += '<h3>' + item.commit.author.date + '</h3>';
+            newBody += '<p>' + item.commit.message + '</p>';
+          });
+          document.getElementById("bodyhtml").innerHTML = newBody;
+
+          previousRequest = data;
+        }
+      })
+      .catch(function(err) {
+        console.warn('Something went wrong.', err);
       });
-      document.body.innerHTML = newBody;
-    })
-    .catch(function(err) {
-      // There was an error
-      console.warn('Something went wrong.', err);
-    });
+    }
 }
